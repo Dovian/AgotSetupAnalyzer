9222a28da95841a1431480b91d4f3aaa383eafa7
Daniel <deboerdovian@gmail.com>
2016-10-31 23:20:36 -0500

    Adding some possible analyzer code

--- AGoTSetupAnalyzer/AgotSetupAnalyzer/DeckAnalyzer.cs
+++ AGoTSetupAnalyzer/AgotSetupAnalyzer/DeckAnalyzer.cs
@@ -25,12 +25,73 @@ namespace AgotSetupAnalyzer
             };
 
             deck.Shuffle();
-            throw new NotImplementedException();
+            var hand = deck.DeckList.Take(7);
+
+            return new AnalyzerResultsDTO();
+        }
+
+        //Likely best sort for most used
+        private List<Card> PickForMostCardsUsed(List<Card> hand)
+        {
+            var goldRemaining = StaticValues.SetupGold;
+            List<Card> setup = new List<Card>();
+
+            var handCopy = hand.OrderBy(c => c.Cost).ToList();
+            
+            while (goldRemaining > 0)
+            {
+                goldRemaining -= handCopy.First().Cost;
+
+                if (goldRemaining >= 0)
+                    setup.Concat(handCopy.Take(1));
+            }
+
+            /*Check replacements (does goldRemaining + cost = another character's cost?)*/
+
+            return setup;
+        }
+
+
+        //Possible sort for closest to 8
+        private List<Card> PickForClosestToCap(List<Card> hand)
+        {
+            var goldRemaining = StaticValues.SetupGold;
+            List<Card> setup = new List<Card>();
+            var possibleSetups = new List<List<Card>>();
+
+
+            for (int i = 7; i > 0; i++)
+            {
+                var handCopy = hand.OrderBy(c => c.Cost);
+                if (handCopy.Any(c => c.Cost == i))
+                {
+                    var possibleSetup = new List<Card>();
+                    possibleSetup.Add(handCopy.First(c => c.Cost == i));
+
+                    goldRemaining -= i;
+
+                    while (goldRemaining > 0)
+                    {
+                        goldRemaining -= handCopy.First().Cost;
+
+                        if (goldRemaining >= 0)
+                            possibleSetup.Concat(handCopy.Take(1));
+                    }
+
+                    possibleSetups.Add(possibleSetup);
+                }
+
+                goldRemaining = 8;
+            }
+
+            /*Pick from possibles*/
+
+            return setup;
         }
 
         private string[] ParseThronesDbList(string thronesDbList)
         {
-            var lines = thronesDbList.Split(new string[] {"\n", "\r\n"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var lines = thronesDbList.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
             List<int> linesToRemove = new List<int>();
             for (int i = 0; i < lines.Count; i++)
                 if (lines[i].First() != '1' && lines[i].First() != '2' && lines[i].First() != '3')
