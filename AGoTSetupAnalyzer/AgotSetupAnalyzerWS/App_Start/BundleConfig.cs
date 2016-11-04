5e83fcf1ba3366b0bdd488e54475dfd178a10d4b
Dovian <deboerdovian@gmail.com>
2016-11-04 16:24:28 -0500

    Updated tons of UI stuff
    Lots of charts added and tweaked to look nice.

--- AGoTSetupAnalyzer/AgotSetupAnalyzerWS/App_Start/BundleConfig.cs
+++ AGoTSetupAnalyzer/AgotSetupAnalyzerWS/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace AgotSetupAnalyzerWS
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+                "~/Scripts/Chart.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
