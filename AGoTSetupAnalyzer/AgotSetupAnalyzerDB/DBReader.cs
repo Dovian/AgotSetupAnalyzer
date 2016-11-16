c69a385fb36314eeb4120593eeb03f5e64e7147a
Dovian <deboerdovian@gmail.com>
2016-11-16 09:30:18 -0600

    Fixed second apostrophe character in SQL query

--- AGoTSetupAnalyzer/AgotSetupAnalyzerDB/DBReader.cs
+++ AGoTSetupAnalyzer/AgotSetupAnalyzerDB/DBReader.cs
@@ -25,7 +25,7 @@ namespace AgotSetupAnalyzerDB
             string connectionString = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
                     config.Server, config.Port, config.User, config.Password, config.Database);
 
-            string sql = String.Format("SELECT * FROM \"agotCardTable\" WHERE name='{0}'", cardName.Replace("'", "''"));
+            string sql = String.Format("SELECT * FROM \"agotCardTable\" WHERE name='{0}'", cardName.Replace("'", "''").Replace("’", "''"));
             if (!string.IsNullOrEmpty(setCode))
                 sql += String.Format(" AND \"packCode\"='{0}'", setCode);
 
